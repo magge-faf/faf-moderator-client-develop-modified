30b245565e3eea0c04042512d2d4158d2c073c5c
magge-faf <benspott+github@gmail.com>
2023-01-16 18:27:01 +0100

    feat: Added time tracking in title

--- src/main/java/com/faforever/moderatorclient/FafModeratorClientApplication.java
+++ src/main/java/com/faforever/moderatorclient/FafModeratorClientApplication.java
@@ -7,6 +7,7 @@ import com.faforever.moderatorclient.ui.PlatformServiceImpl;
 import com.faforever.moderatorclient.ui.StageHolder;
 import com.faforever.moderatorclient.ui.UiService;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.text.Font;
@@ -18,6 +19,8 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableConfigurationProperties(ApplicationProperties.class)
@@ -47,11 +50,39 @@ public class FafModeratorClientApplication extends Application {
         UiService uiService = applicationContext.getBean(UiService.class);
         MainController mainController = uiService.loadFxml("ui/mainWindow.fxml");
         mainController.display();
-        primaryStage.getIcons().add(new Image(this.getClass().getResourceAsStream("/media/favicon.png")));
+        primaryStage.getIcons().add(new Image(Objects.requireNonNull(this.getClass().getResourceAsStream("/media/favicon.png"))));
         Scene scene = new Scene(mainController.getRoot());
-        scene.getStylesheets().add(getClass().getResource("/style/main.css").toExternalForm());
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/style/main.css")).toExternalForm());
         primaryStage.setScene(scene);
         primaryStage.show();
+        startTimerThread(primaryStage);
+    }
+
+    private void startTimerThread(Stage primaryStage) {
+        long startTime = System.currentTimeMillis();
+        Thread timerThread = new Thread(() -> {
+            while (true) {
+                long elapsedTime = System.currentTimeMillis() - startTime;
+                log.debug(String.valueOf(elapsedTime));
+                String elapsedTimeStr = String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(elapsedTime),
+                        TimeUnit.MILLISECONDS.toMinutes(elapsedTime) % TimeUnit.HOURS.toMinutes(1),
+                        TimeUnit.MILLISECONDS.toSeconds(elapsedTime) % TimeUnit.MINUTES.toSeconds(1));
+                if (elapsedTime >= 68000 && elapsedTime <= 69000) {
+                    Platform.runLater(() -> primaryStage.setTitle("nice"));
+                } else if (elapsedTime >= 419000 && elapsedTime <= 420000) {
+                    Platform.runLater(() -> primaryStage.setTitle("( *∀*)y─┛"));
+                } else {
+                    Platform.runLater(() -> primaryStage.setTitle("magge's modified Mordor - Running Time: " + elapsedTimeStr));
+                }
+                try {
+                    // Sleep cycle has a slight delay by ~5ms for executing code. Not important to have it that accurate
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        timerThread.start();
     }
 
     @Bean
