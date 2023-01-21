4cfc787ff5663a0b5fb3d8964d06cd10f1607dfd
magge-faf <benspott+github@gmail.com>
2023-01-21 17:45:07 +0100

    style: Replace old feature statistics, cut old code, reorganized visual elements

--- src/main/java/com/faforever/moderatorclient/ui/main_window/SettingsController.java
+++ src/main/java/com/faforever/moderatorclient/ui/main_window/SettingsController.java
@@ -1,9 +1,7 @@
 package com.faforever.moderatorclient.ui.main_window;
 
-import com.faforever.moderatorclient.ui.moderation_reports.ModerationReportController;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
@@ -12,17 +10,10 @@ import javafx.fxml.FXML;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import java.awt.*;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.*;
-import java.awt.datatransfer.StringSelection;
-import java.awt.datatransfer.Clipboard;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 
 @Component
 @Slf4j
@@ -41,12 +32,9 @@ public class SettingsController implements Controller<Region> {
     public Button BlacklistedUUID;
     public Button BlacklistedVolumeSN;
     public Button ExcludedItems;
-    public TextField allModeratorStatsTextField;
     public Button templateCompletedButton;
     public Button templateDiscardedButton;
     public Button templateReportButton;
-    public TextArea mostReportsOffendersTextArea;
-    public Button generateReportsAndModeratorStatisticsButton;
     public TextField genericJunk;
     public MenuItem optionUserManagementTab;
     public MenuItem optionReportTab;
@@ -158,158 +146,23 @@ public class SettingsController implements Controller<Region> {
         pb.start();
     }
 
-    public void BlacklistedHash() throws IOException {
-        openFile("BlacklistedHash.txt");
-    }
+    public void BlacklistedHash() throws IOException {openFile("BlacklistedHash.txt");}
 
-    public void BlacklistedIP() throws IOException {
-        openFile("BlacklistedIP.txt");
-    }
+    public void BlacklistedIP() throws IOException {openFile("BlacklistedIP.txt");}
 
-    public void BlacklistedSN() throws IOException {
-        openFile("BlacklistedSN.txt");
-    }
+    public void BlacklistedSN() throws IOException {openFile("BlacklistedSN.txt");}
 
-    public void BlacklistedUUID() throws IOException {
-        openFile("BlacklistedUUID.txt");
-    }
+    public void BlacklistedUUID() throws IOException {openFile("BlacklistedUUID.txt");}
 
-    public void BlacklistedVolumeSN() throws IOException {
-        openFile("BlacklistedVolumeSN.txt");
-    }
+    public void BlacklistedVolumeSN() throws IOException {openFile("BlacklistedVolumeSN.txt");}
 
-    public void BlacklistedMemorySN() throws IOException {
-        openFile("BlacklistedMemorySN.txt");
-    }
+    public void BlacklistedMemorySN() throws IOException {openFile("BlacklistedMemorySN.txt");}
 
-    public void ExcludedItems() throws IOException {
-        openFile("excludedItems.txt");
-    }
+    public void ExcludedItems() throws IOException {openFile("excludedItems.txt");}
 
-    public void TemplateCompletedButton() throws IOException {
-        openFile("TemplateCompleted.txt");
-    }
+    public void TemplateCompletedButton() throws IOException {openFile("TemplateCompleted.txt");}
 
-    public void TemplateDiscardedButton() throws IOException {
-        openFile("TemplateDiscarded.txt");
-    }
+    public void TemplateDiscardedButton() throws IOException {openFile("TemplateDiscarded.txt");}
 
-    public void TemplateReportButton() throws IOException {
-        openFile("TemplateReport.txt");
-    }
-
-    int extractInt(String s) {
-        Pattern pattern = Pattern.compile("\\d+");
-        Matcher matcher = pattern.matcher(s);
-        if (matcher.find()) {
-            return Integer.parseInt(matcher.group());
-        } else {
-            return 0;
-        }
-    }
-
-    public void generateReportsAndModeratorStatistics() {
-        if(!ModerationReportController.GlobalConstants.allReports.isEmpty()){
-            try {
-                String allReportsString = String.valueOf(ModerationReportController.GlobalConstants.allReports);
-                allReportsString = allReportsString.replaceAll("\\[.*?]",""); // remove []
-                String[] dataList = allReportsString.split(",");
-                List<String> Moderator = new ArrayList<>(Collections.singletonList(""));
-
-                for (String item : dataList) {
-                    item = Arrays.toString(item.split(":"));
-                    Moderator.add(Arrays.toString(new String[]{item.split(",")[1]}));
-                }
-
-                Set<String> unique = new HashSet<>(Moderator);
-                List<String> totalProcessedReportsRaw = new ArrayList<>();
-                List<String> totalProcessedReportsProcessed = new ArrayList<>();
-
-                for (String key : unique) {
-                    if (!key.equals("")){
-                        totalProcessedReportsRaw.add(key + ": " + Collections.frequency(Moderator, key));
-                    }
-                }
-
-                for (String item : totalProcessedReportsRaw) {
-                    item = item.replaceAll("\\[", "").replaceAll("]", "").replaceAll(" ", "");
-                    totalProcessedReportsProcessed.add(item);
-                }
-
-                totalProcessedReportsProcessed.sort((o1, o2) -> {
-                    // Extract the number of reports processed from each string
-                    int num1 = extractInt(o1);
-                    int num2 = extractInt(o2);
-                    // Compare the numbers
-                    return num1 - num2;
-                });
-
-
-                List<String> finalList = new ArrayList<>();
-
-                for (String item : totalProcessedReportsProcessed){
-                    if (!item.contains("DISCARDED")){  // ignore legacy reports which has DISCARDED as moderator name
-                        finalList.add(item);
-                    }
-                }
-
-                List<?> shallowCopy = finalList.subList(0, finalList.size());
-                Collections.reverse(shallowCopy);
-
-                allModeratorStatsTextField.setText(String.valueOf(finalList));
-
-                String AwaitingReportsTotalTextAreaString = String.valueOf(ModerationReportController.GlobalConstants.AwaitingReportsTotalTextArea);
-
-                List<String> allOffendersListGlobal = ModerationReportController.GlobalConstants.allOffenders;
-                List<String> allRUOffendersList = ModerationReportController.GlobalConstants.allRUOffenders;
-
-                allOffendersListGlobal.removeAll(allRUOffendersList);
-                Map<String, Integer> offenderCounts = new HashMap<>();
-                for (String offender : allOffendersListGlobal) {
-                    offenderCounts.put(offender, offenderCounts.getOrDefault(offender, 0) + 1);
-                }
-
-                for (Map.Entry<String, Integer> entry : offenderCounts.entrySet()) {
-                    String offender = entry.getKey();
-                    int frequency = entry.getValue();
-                    String text = offender + " : " + frequency;
-                    if (frequency > 3) {
-                        mostReportsOffendersTextArea.appendText(text + "\n");
-
-                    }
-                }
-
-                // Count the frequency of offenders in allRUOffendersList
-                Map<String, Integer> ruOffenderCounts = new HashMap<>();
-                for (String offender : allRUOffendersList) {
-                    ruOffenderCounts.put(offender, ruOffenderCounts.getOrDefault(offender, 0) + 1);
-                }
-
-                for (Map.Entry<String, Integer> entry : ruOffenderCounts.entrySet()) {
-                    String offender = entry.getKey();
-                    int frequency = entry.getValue();
-                    String text = offender + ": " + frequency + " -> RU";
-                    if (frequency > 3) {
-                        mostReportsOffendersTextArea.appendText(text + "\n");
-                    }
-                }
-                // Concatenate the moderator stats, awaiting reports, and repeat offenders and paste into clipboard
-                String myString = allModeratorStatsTextField.getText() + "\n\n"
-                        + AwaitingReportsTotalTextAreaString + "\n\n"
-                        + "Repeat offenders:\n\n"
-                        + mostReportsOffendersTextArea.getText();
-
-                StringSelection stringSelection = new StringSelection(myString);
-                Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-                clipboard.setContents(stringSelection, null);
-
-                ModerationReportController.GlobalConstants.allReports.clear();
-                ModerationReportController.GlobalConstants.allOffenders.clear();
-                ModerationReportController.GlobalConstants.allRUOffenders.clear();
-
-            }catch (Exception e) {
-                allModeratorStatsTextField.setText(String.valueOf(e));
-            }
-        }
-    }
+    public void TemplateReportButton() throws IOException {openFile("TemplateReport.txt");}
 }
