2c8cfe7c92d4ad3d41f0b8f83a52bb5ba6ca8238
magge-faf <101107758+magge-faf@users.noreply.github.com>
2024-11-21 02:02:11 +0100

    fix: bring back 'Add ban' button in report view

--- src/main/java/com/faforever/moderatorclient/ui/ViewHelper.java
+++ src/main/java/com/faforever/moderatorclient/ui/ViewHelper.java
@@ -810,27 +810,7 @@ public class ViewHelper {
         tableView.getColumns().add(updateTimeColumn);
         */
 
-                    if (onAddBan != null) {
-                        TableColumn<PlayerFX, PlayerFX> banOptionColumn = new TableColumn<>("Ban");
-                        banOptionColumn.setCellValueFactory(param -> new SimpleObjectProperty<>(param.getValue()));
-                        banOptionColumn.setCellFactory(param -> new TableCell<>() {
 
-                            @Override
-                            protected void updateItem(PlayerFX item, boolean empty) {
-                                super.updateItem(item, empty);
-                                if (!empty) {
-                                    if (!item.isBannedGlobally()) {
-                                        Button button = new Button("Add ban");
-                                        button.setOnMouseClicked(event -> onAddBan.accept(item));
-                                        setGraphic(button);
-                                        return;
-                                    }
-                                }
-                                setGraphic(null);
-                            }
-                        });
-                        tableView.getColumns().add(banOptionColumn);
-                    }
 
                     TableColumn<PlayerFX, String> hashColumn = new TableColumn<>("Hash");
                     hashColumn.setCellValueFactory(o -> Bindings.createStringBinding(() ->
@@ -1008,6 +988,28 @@ public class ViewHelper {
                     extractors.put(volumeSerialNumber, playerFX -> playerFX.getUniqueIds().stream().map(UniqueIdFx::getVolumeSerialNumber).collect(Collectors.toList()));
 
                 }
+            }
+
+            if (onAddBan != null) {
+                TableColumn<PlayerFX, PlayerFX> banOptionColumn = new TableColumn<>("Ban");
+                banOptionColumn.setCellValueFactory(param -> new SimpleObjectProperty<>(param.getValue()));
+                banOptionColumn.setCellFactory(param -> new TableCell<>() {
+
+                    @Override
+                    protected void updateItem(PlayerFX item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (!empty) {
+                            if (!item.isBannedGlobally()) {
+                                Button button = new Button("Add ban");
+                                button.setOnMouseClicked(event -> onAddBan.accept(item));
+                                setGraphic(button);
+                                return;
+                            }
+                        }
+                        setGraphic(null);
+                    }
+                });
+                tableView.getColumns().add(banOptionColumn);
             }
         }
 
